95a396cda3d4984966bb3c2dd9766174a214ed04
daixuan <337568659@qq.com>
2019-04-05 01:16:17 +0800

    初始化仓库

---
+++ src/main/java/cn/gorillahug/back/common/utils/CSVUtils.java
@@ -0,0 +1,17 @@
+package cn.gorillahug.back.common.utils;
+
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+@Slf4j
+public class CSVUtils {
+
+
+}
+
---
+++ src/main/java/cn/gorillahug/back/common/utils/FileUtils.java
@@ -0,0 +1,120 @@
+package cn.gorillahug.back.common.utils;
+
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+@Slf4j
+public class FileUtils {
+
+    public static boolean deleteFile(String fileName) {
+        File file = new File(fileName);
+        // 如果文件路径所对应的文件存在，并且是一个文件，则直接删除
+        if (file.exists() && file.isFile()) {
+            if (file.delete()) {
+                log.info("删除单个文件" + fileName + "成功！");
+                return true;
+            } else {
+                log.info("删除单个文件" + fileName + "失败！");
+                return false;
+            }
+        } else {
+            log.info("删除单个文件失败：" + fileName + "不存在！");
+            return false;
+        }
+    }
+
+    /**
+     * zip压缩
+     *
+     * @param sourceFileName 源文件名
+     * @param zipFileName    目标文件名
+     * @throws Exception
+     */
+    public static void zip(String sourceFileName, String zipFileName) throws Exception {
+        log.info("压缩中...");
+
+        //创建zip输出流
+        ZipOutputStream out = new ZipOutputStream(new FileOutputStream(zipFileName));
+
+        //创建缓冲输出流
+        BufferedOutputStream bos = new BufferedOutputStream(out);
+
+        File sourceFile = new File(sourceFileName);
+
+        compress(out, bos, sourceFile, sourceFile.getName());
+
+        bos.close();
+        out.close();
+
+        log.info("压缩完成");
+    }
+
+    public static void compress(ZipOutputStream out, BufferedOutputStream bos, File sourceFile, String base) throws Exception {
+        //如果路径为目录（文件夹）
+        if (sourceFile.isDirectory()) {
+            //取出文件夹中的文件（或子文件夹）
+            File[] fList = sourceFile.listFiles();
+
+            if (fList.length == 0)//如果文件夹为空，则只需在目的地zip文件中写入一个目录进入点
+            {
+                System.out.println(base + "/");
+                out.putNextEntry(new ZipEntry(base + "/"));
+            } else//如果文件夹不为空，则递归调用compress，文件夹中的每一个文件（或文件夹）进行压缩
+            {
+                for (int i = 0; i < fList.length; i++) {
+                    compress(out, bos, fList[i], base + "/" + fList[i].getName());
+                }
+            }
+        } else//如果不是目录（文件夹），即为文件，则先写入目录进入点，之后将文件写入zip文件中
+        {
+            out.putNextEntry(new ZipEntry(base));
+            FileInputStream fos = new FileInputStream(sourceFile);
+            BufferedInputStream bis = new BufferedInputStream(fos);
+
+            int tag;
+            log.info(base);
+            //将源文件写入到zip文件中
+            while ((tag = bis.read()) != -1) {
+                bos.write(tag);
+            }
+            bis.close();
+            fos.close();
+
+        }
+    }
+
+    public static void readCSV(String path) {
+        File csv = new File(path);  // CSV文件路径
+        BufferedReader br = null;
+        try {
+            br = new BufferedReader(new FileReader(csv));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        String line;
+        String everyLine;
+        try {
+            List<String> allString = new ArrayList<>();
+            while ((line = br.readLine()) != null) {
+                if (line.indexOf(",,,,,,,,,,,") != -1) {
+                    log.info("------------11-----------");
+                    continue;
+                }
+                everyLine = line;
+                System.out.println(everyLine);
+                allString.add(everyLine);
+            }
+            System.out.println("csv表格中所有行数：" + allString.size());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
+
